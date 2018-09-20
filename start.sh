0c652ca87437a66ca3bccd38fd0fee3cc7c884f9
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 22:08:29 -0500

    parse the player headers from the tv curl

--- start.sh
+++ start.sh
@@ -7,6 +7,32 @@ logpath=/cache/log.txt
 mkdir -p /cache
 
 while true; do
-    curl -sSL https://tv.mechmania.io > $logpath
-    $VISUALIZER $logpath
+    get_team1=0
+    get_team2=0
+    for header in `curl -sSL -D - https://tv.mechmania.io -o $logpath`; do
+        # If the previous line was a header, grab the team name
+        if [ $get_team1 -eq 1 ]; then
+            team1=$header
+        fi;
+        if [ $get_team2 -eq 1 ]; then
+            team2=$header
+        fi;
+
+        # Set the "grabber" flag back to default
+        get_team1=0
+        get_team2=0
+
+        # Check if the header is what we wanna look for
+        if [ $header = "x-team-1:" ]; then
+            get_team1=1;
+        fi;
+        if [ $header = "x-team-2:" ]; then
+            get_team2=1;
+        fi;
+    done;
+
+    echo "Starting game"
+    echo "Team 1: $team1"
+    echo "Team 2: $team2"
+    $VISUALIZER $logpath $team1 $team2
 done;
